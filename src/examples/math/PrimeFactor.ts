33bf3e845b08152829c7074080e100ef3f13aadc
Kvaigone <cirkovs508@gmail.com>
2022-10-30 22:30:40 +0300

    Add math examples directory

---
+++ src/examples/math/PrimeFactor.ts
@@ -0,0 +1,22 @@
+
+
+function getPrimeFactorArray(range: number) : Array<number> {
+    let result : number[] = [];
+    for (let k = 2; k < range; k++) {
+        let hasDivider = false;
+        for (let i = 2; i < range; i++) {
+            if (k === i) break;
+            if (k % i === 0) {
+                hasDivider = true;
+                break;
+            }
+        }
+
+        if (!hasDivider) {
+            result.push(k);
+        }
+    }
+    return result;
+}
+
+console.log(getPrimeFactorArray(300));
